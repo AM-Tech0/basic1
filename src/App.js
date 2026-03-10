48c1fa2853bca0054e0d55c81d4036b85e0999a3
AM-Tech0 <amanbsdm6794@gmail.com>
2026-03-10 17:03:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  state ={count:0};
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     count: 0
+  //   };
+  // }
+  increment=()=> {
+    this.setState({ count: this.state.count + 1 });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  decrement=()=> {
+    if(this.state.count>0){
+       this.setState({ count: this.state.count - 1 });
+    }
+   
+  }
+  render() {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Tailwind CSS is working 🚀
+        </h1>
+        <div className='flex flex-col m-6 gap-5  '>
+          <h1  className="text-3xl font-bold text-center">Count :{this.state.count}</h1>
+          <div className='flex gap-2'>
+            <button className="px-4 py-2 bg-green-500 text-white rounded" onClick={this.increment}>Increment</button>
+          <button  className="px-4 py-2 bg-red-500 text-white rounded" onClick={this.decrement}>Decrement</button>
+          </div> 
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
+// import React, { Component } from "react";
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       count: 0
+//     };
+//   }
+
+//   increment = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
+
+//   decrement = () => {
+//     this.setState({ count: this.state.count - 1 });
+//   };
+
+//   render() {
+//     return (
+//       <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+//         <h1 className="text-3xl font-bold">Count: {this.state.count}</h1>
+
+//         <button
+//           onClick={this.increment}
+//           className="px-4 py-2 bg-green-500 text-white rounded"
+//         >
+//           Increment
+//         </button>
+
+//         <button
+//           onClick={this.decrement}
+//           className="px-4 py-2 bg-red-500 text-white rounded"
+//         >
+//           Decrement
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
